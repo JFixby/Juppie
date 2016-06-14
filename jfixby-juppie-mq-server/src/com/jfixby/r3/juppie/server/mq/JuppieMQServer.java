166cc83ddaa363ceb2f68b719780222cfdfa57eb
J Fixby <github@jfixby.com>
2016-06-14 23:14:30 +0200

    srv

---
+++ jfixby-juppie-mq-server/src/com/jfixby/r3/juppie/server/mq/JuppieMQServer.java
@@ -0,0 +1,10 @@
+
+package com.jfixby.r3.juppie.server.mq;
+
+public class JuppieMQServer {
+
+	public static JuppieMQServerSpecs newJuppieMQServerSpecs () {
+		return new JuppieMQServerSpecs();
+	}
+
+}
---
+++ jfixby-juppie-mq-server/src/com/jfixby/r3/juppie/server/mq/JuppieMQServerSpecs.java
@@ -0,0 +1,5 @@
+package com.jfixby.r3.juppie.server.mq;
+
+public class JuppieMQServerSpecs {
+
+}
