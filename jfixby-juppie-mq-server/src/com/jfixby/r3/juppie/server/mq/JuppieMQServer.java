113d43560d1ee3e633327b435ae5f7a1cb618273
J Fixby <github@jfixby.com>
2016-06-15 23:23:03 +0200

    constructor

--- jfixby-juppie-mq-server/src/com/jfixby/r3/juppie/server/mq/JuppieMQServer.java
+++ jfixby-juppie-mq-server/src/com/jfixby/r3/juppie/server/mq/JuppieMQServer.java
@@ -1,10 +1,40 @@
 
 package com.jfixby.r3.juppie.server.mq;
 
-public class JuppieMQServer {
+import com.jfixby.r3.api.ui.unit.Unit;
+import com.jfixby.r3.api.ui.unit.UnitManager;
+
+public class JuppieMQServer implements Unit {
+
+	private JuppieMQServer (final JuppieMQServerSpecs specs) {
+	}
+
+	public JuppieMQServer newJuppieMQServer (final JuppieMQServerSpecs specs) {
+		return new JuppieMQServer(specs);
+	}
 
 	public static JuppieMQServerSpecs newJuppieMQServerSpecs () {
 		return new JuppieMQServerSpecs();
+	}
+
+	@Override
+	public void onCreate (final UnitManager unitManager) {
+	}
+
+	@Override
+	public void onStart () {
+	}
+
+	@Override
+	public void onResume () {
+	}
+
+	@Override
+	public void onPause () {
+	}
+
+	@Override
+	public void onDestroy () {
 	}
 
 }
